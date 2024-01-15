228f2eb29fe648eb1b7e33a4ee4cb542177a11bc
Catdee123 <31316752+Catdee123@users.noreply.github.com>
2024-01-14 18:32:13 -0600

    initial commit

---
+++ cowboy-showdown.js
@@ -0,0 +1,21 @@
+const treasure = (clickedBox) => {
+ if(clickedBox === 1) {
+    document.getElementById(1).innerHTML = "🎯"
+  } else if(clickedBox === 2) {
+    document.getElementById(2).innerHTML = "🎉"
+  } else if(clickedBox === 3) {
+    document.getElementById(3).innerHTML = "🎯"
+  } else if(clickedBox === 4) {
+    document.getElementById(4).innerHTML = "🎉"
+  } else if(clickedBox === 5) {
+    document.getElementById(5).innerHTML = "🎪"
+  } else if(clickedBox === 6) {
+    document.getElementById(6).innerHTML = "🎯"
+  } else if(clickedBox === 7) {
+    document.getElementById(7).innerHTML = "😎"
+  } else if(clickedBox === 8) {
+    document.getElementById(8).innerHTML = "😎"
+  } else if(clickedBox === 9) {
+    document.getElementById(9).innerHTML = "😎"
+}
+}
